2269dce64293897b4ec34979178eb5fc29c2420f
bofang <libofangfang@163.com>
2018-04-26 17:29:00 +0900

    add unit test for create vocab on annotated corpus and create n-gram vocab

--- tests/test_vocab.py
+++ tests/test_vocab.py
@@ -5,7 +5,7 @@ import runpy
 import io
 import sys
 import contextlib
-from vecto.vocabulary import create_from_dir, create_from_file, create_from_annotated_dir, Vocabulary
+from vecto.vocabulary import create_from_dir, create_from_file, create_from_annotated_dir, create_ngram_tokens_from_dir, Vocabulary
 
 path_text = "./tests/data/corpora/plain"
 annotated_text = "./tests/data/corpora/annotated/"
@@ -32,10 +32,19 @@ class Tests(unittest.TestCase):
         assert vocab.get_id("the") >= 0
 
     def test_create_from_annotated_dir(self):
-        vocab = create_from_annotated_dir(annotated_text, min_frequency=10)
+        for representation in ['word', 'pos', 'deps']:
+            vocab = create_from_annotated_dir(annotated_text, min_frequency=10, representation=representation)
+            print("the:", vocab.get_id("the"))
+            assert vocab.get_id("the") >= 0
+            vocab.save_to_dir("/tmp/vecto/vocab_annotated/")
+            vocab = create_from_file(path_text_file, min_frequency=10)
+            assert vocab.get_id("the") >= 0
+
+    def test_create_ngram_tokens_from_dir(self):
+        vocab = create_ngram_tokens_from_dir(path_text, 1, 3, min_frequency=10)
         print("the:", vocab.get_id("the"))
         assert vocab.get_id("the") >= 0
-        vocab.save_to_dir("/tmp/vecto/vocab_annotated/")
+        vocab.save_to_dir("/tmp/vecto/vocab")
         vocab = create_from_file(path_text_file, min_frequency=10)
         assert vocab.get_id("the") >= 0
 
