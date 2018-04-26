2d6f613370686a1aaaa427459ab736e4b6e36b29
Aleksandr Drozd <alexander.drozd@gmail.com>
2018-04-26 13:31:23 +0900

    more tests for vocab

--- tests/test_vocab.py
+++ tests/test_vocab.py
@@ -1,9 +1,10 @@
 """Tests for vocabulary module."""
 
 import unittest
-from vecto.vocabulary import create_from_dir, Vocabulary
+from vecto.vocabulary import create_from_dir, create_from_file, Vocabulary
 
 path_text = "./tests/data/corpora/plain"
+path_text_file = "./tests/data/corpora/plain/sense_small.txt"
 path_vocab = "./tests/data/vocabs/plain"
 
 
@@ -14,6 +15,8 @@ class Tests(unittest.TestCase):
         print("the:", vocab.get_id("the"))
         assert vocab.get_id("the") >= 0
         vocab.save_to_dir("/tmp/vecto/vocab")
+        vocab = create_from_file(path_text_file, min_frequency=10)
+        assert vocab.get_id("the") >= 0
 
     def test_load_from_dir(self):
         vocab = Vocabulary()
@@ -26,3 +29,9 @@ class Tests(unittest.TestCase):
         tokens = ["the", "apple"]
         ids = vocab.tokens_to_ids(tokens)
         print("ids:", ids)
+
+    def test_misc(self):
+        vocab = Vocabulary()
+        vocab.load(path_vocab)
+        vocab.get_word_by_id(1)
+        vocab.get_frequency("apple")
