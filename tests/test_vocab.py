87520d10c31682df406b4317dc6f5376eec06c9d
Aleksandr Drozd <alexander.drozd@gmail.com>
2018-04-26 13:55:32 +0900

    test vocab cli

--- tests/test_vocab.py
+++ tests/test_vocab.py
@@ -1,11 +1,22 @@
 """Tests for vocabulary module."""
 
 import unittest
+import runpy
+import io
+import sys
+import contextlib
 from vecto.vocabulary import create_from_dir, create_from_file, Vocabulary
 
 path_text = "./tests/data/corpora/plain"
 path_text_file = "./tests/data/corpora/plain/sense_small.txt"
 path_vocab = "./tests/data/vocabs/plain"
+
+
+def run_module(name: str, *args, run_name: str = '__main__') -> None:
+    backup_sys_argv = sys.argv
+    sys.argv = [name + '.py'] + list(args)
+    runpy.run_module(name, run_name=run_name)
+    sys.argv = backup_sys_argv
 
 
 class Tests(unittest.TestCase):
@@ -35,3 +46,10 @@ class Tests(unittest.TestCase):
         vocab.load(path_vocab)
         vocab.get_word_by_id(1)
         vocab.get_frequency("apple")
+
+    def test_cli(self):
+        sio = io.StringIO()
+        with contextlib.redirect_stderr(sio):
+            with self.assertRaises(ValueError):
+                run_module('version_query', '-p', '-i', '.')
+        # _LOG.info('%s', sio.getvalue())
