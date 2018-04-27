200b02a870aabeffff5d0a60c1a269d63d3b16ee
bofang <libofangfang@163.com>
2018-04-27 11:18:20 +0900

    unit test for vocabulary (100% test coverage achieved)

--- tests/test_vocab.py
+++ tests/test_vocab.py
@@ -10,7 +10,7 @@ from vecto.vocabulary import create_from_dir, create_from_file, create_from_anno
     Vocabulary
 
 path_text = "./tests/data/corpora/plain"
-annotated_text = "./tests/data/corpora/annotated/"
+path_annotated_text = "./tests/data/corpora/annotated/"
 
 path_text_file = "./tests/data/corpora/plain/sense_small.txt"
 path_vocab = "./tests/data/vocabs/plain"
@@ -27,22 +27,35 @@ def run_module(name: str, args, run_name: str = '__main__') -> None:
 class Tests(unittest.TestCase):
 
     def test_create_from_dir(self):
-        vocab = create_from_dir(path_text, min_frequency=10)
+        with self.assertRaises(RuntimeError):
+            create_from_dir("./random/empty/")
+
+        vocab = create_from_dir(path_text)
         print("the:", vocab.get_id("the"))
         assert vocab.get_id("the") >= 0
         vocab.save_to_dir("/tmp/vecto/vocab")
-        vocab = create_from_file(path_text_file, min_frequency=10)
+        with self.assertRaises(RuntimeError):
+            create_from_file("./random/empty/file")
+        vocab = create_from_file(path_text_file)
         assert vocab.get_id("the") >= 0
 
     def test_create_from_annotated_dir(self):
+        with self.assertRaises(RuntimeError):
+            vocab = create_from_annotated_dir(path_text)
+        with self.assertRaises(RuntimeError):
+            create_from_annotated_dir("./random/empty/")
+        with self.assertRaises(RuntimeError):
+            create_from_annotated_dir(path_annotated_text, representation='undefined')
         for representation in ['word', 'pos', 'deps']:
-            vocab = create_from_annotated_dir(annotated_text, min_frequency=10, representation=representation)
+            vocab = create_from_annotated_dir(path_annotated_text, representation=representation)
             print("the/det:", vocab.get_id("the/det"))
             assert vocab.get_id("the") >= 0 or vocab.get_id("the/det") >= 0 or vocab.get_id("the/+det")
             vocab.save_to_dir(os.path.join("/tmp/vecto/vocab_annotated/", representation))
 
     def test_create_ngram_tokens_from_dir(self):
-        vocab = create_ngram_tokens_from_dir(path_text, 1, 3, min_frequency=10)
+        with self.assertRaises(RuntimeError):
+            create_ngram_tokens_from_dir("./random/empty/", 1, 3)
+        vocab = create_ngram_tokens_from_dir(path_text, 1, 3)
         print("the:", vocab.get_id("the"))
         assert vocab.get_id("the") >= 0
         vocab.save_to_dir("/tmp/vecto/vocab_ngram/")
@@ -75,7 +88,7 @@ class Tests(unittest.TestCase):
         with contextlib.redirect_stderr(sio):
             # with self.assertRaises(SystemExit):
             run_module('vecto.vocabulary', ['--type', 'normal', '--path_corpus', path_text, '--path_out', '/tmp/vecto/vocabulary/main/normal'])
-            run_module('vecto.vocabulary', ['--type', 'annotated', '--path_corpus', annotated_text, '--path_out', '/tmp/vecto/vocabulary/main/annotated'])
+            run_module('vecto.vocabulary', ['--type', 'annotated', '--path_corpus', path_annotated_text, '--path_out', '/tmp/vecto/vocabulary/main/annotated'])
             run_module('vecto.vocabulary', ['--type', 'ngram_tokens', '--path_corpus', path_text, '--path_out', '/tmp/vecto/vocabulary/main/ngram_tokens'])
             with self.assertRaises(SystemExit):
                 run_module('vecto.vocabulary', '-garbage')
