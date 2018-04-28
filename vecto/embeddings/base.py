a4e009defce1842319c9c89c746908e7732520c5
Aleksandr Drozd <alexander.drozd@gmail.com>
2018-04-28 17:45:58 +0900

    abstract method to get vector

--- vecto/embeddings/base.py
+++ vecto/embeddings/base.py
@@ -1,3 +1,10 @@
-class WordEmbeddings:
+import abc
+from vecto.utils.metadata import WithMetaData
+
+
+class WordEmbeddings(WithMetaData):
     # TODO: define proper interface
-    pass
+
+    @abc.abstractmethod
+    def get_vector(w):
+        pass
