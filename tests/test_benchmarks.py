2c9968ea6e8fd836bcef6c1d911d905322b4ec95
bofang <libofangfang@163.com>
2018-04-30 16:34:31 +0900

    the very first edition of word analogy

--- tests/test_benchmarks.py
+++ tests/test_benchmarks.py
@@ -2,16 +2,18 @@
 
 import unittest
 from vecto.benchmarks.similarity.similarity import Similarity
+from vecto.benchmarks.analogy.analogy import LRCos
 from vecto.embeddings import load_from_dir
 
 
 path_similarity_dataset = "./tests/data/benchmarks/similarity/"
+path_analogy_dataset = "./tests/data/benchmarks/analogy/"
 
 class Tests(unittest.TestCase):
 
 
     def test_similarity(self):
-        embs = load_from_dir("tests/data/embeddings/text/plain_with_file_header")
+        embs = load_from_dir("./tests/data/embeddings/text/plain_with_file_header")
         similarity = Similarity()
         result = similarity.get_result(embs, path_similarity_dataset)
         print(result)
@@ -19,3 +21,11 @@ class Tests(unittest.TestCase):
         similarity = Similarity(ignore_oov=False)
         result = similarity.get_result(embs, path_similarity_dataset)
         print(result)
+
+    def test_analogy(self):
+        embs = load_from_dir("./tests/data/embeddings/text/plain_with_file_header")
+        analogy = LRCos()
+        analogy.get_result(embs, path_analogy_dataset)
+
+
+Tests().test_analogy()
