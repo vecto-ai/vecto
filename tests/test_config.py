c4cac731538e166125d1f42a339992f6f7a30080
Aleksandr Drozd <alexander.drozd@gmail.com>
2018-06-28 23:52:24 +0900

    create config dir if does not exist

--- tests/test_config.py
+++ tests/test_config.py
@@ -7,6 +7,7 @@ class Tests(unittest.TestCase):
 
     def test_file_corpus(self):
         default_dir = os.path.expanduser("~/.vecto/")
+        os.makedirs(default_dir, exist_ok=True)
         path_config = os.path.join(default_dir, 'config.py')
         if not os.path.isfile(path_config):
             with open(path_config, "w") as f:
