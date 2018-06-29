57e2e58dfe995b7fe2feadf4f47bcc8aee44e27b
Aleksandr Drozd <alexander.drozd@gmail.com>
2018-06-29 12:30:26 +0900

    more cli for analogy

--- vecto/benchmarks/analogy/__main__.py
+++ vecto/benchmarks/analogy/__main__.py
@@ -1,9 +1,21 @@
-from .analogy import Analogy
+import argparse
+from vecto.benchmarks.analogy import *
+from vecto.config import load_config
+from vecto.embeddings import load_from_dir
 
 
 def main():
-    print("analogy cli")
-    # parse config
+    # print("analogy cli")
+    config = load_config()
+    # print(config)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("embeddings")
+    parser.add_argument("dataset")
+    parser.add_argument("--path_out", help="destination folder to save results")
+    args = parser.parse_args()
+    embeddings = load_from_dir(args.embeddings)
+    benchmark = LRCos()
+    results = benchmark.get_result(embeddings, args.dataset)
 
 
 if __name__ == "__main__":
