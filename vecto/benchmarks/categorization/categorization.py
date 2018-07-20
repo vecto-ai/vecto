ba164854f64817d239165dd57095823b87219650
bakarov <amirbakarov@gmail.com>
2018-07-20 19:07:39 +0300

    add test set reading

--- vecto/benchmarks/categorization/categorization.py
+++ vecto/benchmarks/categorization/categorization.py
@@ -1,5 +1,7 @@
 from ..base import Benchmark
+from collections import defaultdict
 import csv
+
 
 class Categorization(Benchmark):
     def __init__(self, normalize=True, ignore_oov=True):
@@ -7,4 +9,22 @@ class Categorization(Benchmark):
         self.ignore_oov = ignore_oov
 
     def read_test_set(self, path):
-       pass
+        test = defaultdict(lambda: [])
+        if path.endswith('.csv'):
+            with open(path, 'r') as csvfile:
+                reader = csv.reader(csvfile)
+                head = True
+                for row in reader:
+                    if len(row) < 2:
+                        continue
+                    if not head:
+                        category = row[1]
+                        word = row[2]
+                        test[category].append(word)
+                    head = False
+        else:
+            with open(path) as f:
+                for line in f:
+                    id, category, word = line.strip().split()
+                    test[category].append(word)
+        return test
