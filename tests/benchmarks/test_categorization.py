825c4fd29f490a757078e4d89d68c3b111e58d10
bakarov <amirbakarov@gmail.com>
2018-07-20 18:53:06 +0300

    add initial tests for categorization benchmark

---
+++ tests/benchmarks/test_categorization.py
@@ -0,0 +1,21 @@
+"""Tests for categorization benchmark."""
+
+import contextlib
+import unittest
+import sys
+import io
+from os import path
+from vecto.benchmarks.categorization import *
+from vecto.benchmarks.analogy import visualize as analogy_visualize
+from vecto.embeddings import load_from_dir
+from ..test_setup import run_module
+
+
+path_categorization_dataset = path.join('.', 'tests', 'data', 'benchmarks', 'categorization')
+
+class Tests(unittest.TestCase):
+    def test_analogy(self):
+        embs = load_from_dir(path.join('tests', 'data', 'embeddings', 'text', 'plain_with_file_header'))
+        categorization = Categorization()
+        result = categorization.get_result(embs, categorization)
+        print(result)
