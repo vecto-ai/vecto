12fb294ff0b82aa1bdb542f0d91a95cadaae6362
bakarov <amirbakarov@gmail.com>
2018-07-27 17:21:08 +0300

    add more tests

--- tests/benchmarks/test_synonymy_detection.py
+++ tests/benchmarks/test_synonymy_detection.py
@@ -15,7 +15,6 @@ class Tests(unittest.TestCase):
         embs = load_from_dir(path.join('tests', 'data', 'embeddings', 'text', 'plain_with_file_header'))
         synonymy = CosineDistance()
         result = synonymy.get_result(embs, path_synonymy_dataset)
-        print(result)
 
     def test_cli(self):
         sio = StringIO()
@@ -24,3 +23,26 @@ class Tests(unittest.TestCase):
                        './tests/data/embeddings/text/plain_with_file_header/',
                        './tests/data/benchmarks/synonymy_detection',
                        '--path_out', '/tmp/vecto/', '--method', 'CosineDistance')
+
+    def test_synonymy_results(self):
+        embs = load_from_dir(path.join('tests', 'data', 'embeddings', 'text', 'plain_with_file_header'))
+        synonymy = CosineDistance()
+        result = synonymy.get_result(embs, path_synonymy_dataset)[0]
+        cat_is_synonym = 'yes'
+        cat_is_hit = False
+        distance_to_cat = 1.0
+
+        self.assertEqual(result['tiger'][0]['is_synonym'], 'yes')
+        self.assertEqual(result['tiger'][0]['hit'], False)
+        self.assertEqual(result['tiger'][0]['distance'], distance_to_cat)
+
+    def test_synonymy_reader(self):
+        synonymy = CosineDistance()
+        test_set = synonymy.read_test_set(path.join(path_synonymy_dataset, 'test.csv'))
+        expected_amount_of_keys = 2
+        expected_amount_of_tiger_suspicious = 3
+        cat_is_synonym_with_tiger = 'yes'
+
+        self.assertEqual(len(test_set.keys()), expected_amount_of_keys)
+        self.assertEqual(len(test_set['tiger']), expected_amount_of_tiger_suspicious)
+        self.assertEqual(test_set['tiger'][0][1], cat_is_synonym_with_tiger)
