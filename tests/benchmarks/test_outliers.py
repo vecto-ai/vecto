d4d8326e5c434467ca6e2aba0301560ff578e0c9
bakarov <amirbakarov@gmail.com>
2018-07-27 15:07:37 +0300

    update tests

--- tests/benchmarks/test_outliers.py
+++ tests/benchmarks/test_outliers.py
@@ -14,5 +14,13 @@ path_categorization_dataset = path.join('.', 'tests', 'data', 'benchmarks', 'out
 class Tests(unittest.TestCase):
     def test_categorization(self):
         embs = load_from_dir(path.join('tests', 'data', 'embeddings', 'text', 'plain_with_file_header'))
-        categorization = Outliers()
+        categorization = AveragePairwiseCosine()
         result = categorization.get_result(embs, path_categorization_dataset)
+
+    def test_cli(self):
+        sio = StringIO()
+        with redirect_stdout(sio):
+            run_module('vecto.benchmarks.outliers',
+                       './tests/data/embeddings/text/plain_with_file_header/',
+                       './tests/data/benchmarks/outliers/',
+                       '--path_out', '/tmp/vecto/', '--method', 'AveragePairwiseCosine')
