25d460ca2b7453e8faf90887696142f189e000f4
bakarov <amirbakarov@gmail.com>
2018-07-31 17:01:11 +0300

    cosmetic refactor of similarity benchmark

--- vecto/benchmarks/similarity/similarity.py
+++ vecto/benchmarks/similarity/similarity.py
@@ -19,57 +19,66 @@ class Similarity(Benchmark):
     def __init__(self, normalize=True, ignore_oov=True):
         self.normalize = normalize
         self.ignore_oov = ignore_oov
+        self.test_set_row_length = 3
+        self.should_make_words_lower = False
+        self.default_distance = 0
+        self.minimum_found_pairs_number = 2
+        self.default_result_value = None
+
+    @classmethod
+    def read_test_sample(cls, row, is_csv=True):
+        if is_csv:
+            word_1 = row[0]
+            word_2 = row[1]
+            similarity = row[2]
+        else:
+            word_1, word_2, similarity = row.strip().split()
+        return (word_1, word_2), float(similarity)
 
     def read_test_set(self, path):
         test = []
-        if path.endswith(".csv"):
+        if path.endswith('.csv'):
             with open(path, 'r') as csvfile:
                 reader = csv.reader(csvfile)
                 head = True
                 for row in reader:
-                    if len(row) < 3:
+                    if len(row) < self.test_set_row_length:
                         continue
                     if not head:
-                        x = row[0]
-                        y = row[1]
-                        sim = row[2]
-                        test.append(((x, y), float(sim)))
+                        test.append(self.read_test_set(row))
                     head = False
         else:
             with open(path) as f:
-                for line in f:
-                    # line = line.lower();
-                    x, y, sim = line.strip().split()
-                    test.append(((x, y), float(sim)))
+                for row in f:
+                    test.append(self.read_test_sample(row, is_csv=False))
         return test
 
-    def evaluate(self, embs, data):
+    def make_words_lower(self, word_1, word_2):
+        if self.should_make_words_lower:
+            return word_1.lower(), word_2.lower()
+        return word_1, word_2
+
+    def evaluate(self, embeddings, data):
         details = []
         results = []
-        cnt_found_pairs_total = 0
-        for (x, y), sim in data:
-            x = x.lower()
-            y = y.lower()
-            # print(x,y)
-            if embs.has_word(x) and embs.has_word(y) and not math.isnan(embs.get_vector(x).dot(embs.get_vector(y))):
-                # print(m.get_row(x).dot(m.get_row(y)))
-                v = embs.get_vector(x).dot(embs.get_vector(y))
-                results.append((v, sim))
-                cnt_found_pairs_total += 1
-                details.append([x, y, float(v), float(sim)])
+        found_pairs_number = 0
+        for (word_1, word_2), similarity in data:
+            word_1, word_2 = self.make_words_lower(word_1, word_2)
+            if embeddings.has_word(word_1) and embeddings.has_word(word_2) and not math.isnan(
+                    embeddings.get_vector(word_1).dot(embeddings.get_vector(word_2))):
+                distance = embeddings.get_vector(word_1).dot(embeddings.get_vector(word_2))
+                results.append((distance, similarity))
+                found_pairs_number += 1
+                details.append([word_1, word_2, float(distance), float(similarity)])
             else:
                 if not self.ignore_oov:
-                    # results.append((-1, sim))
-                    # details.append([x, y, str(-1), str(sim)])
-                    results.append((0, sim))
-                    details.append([x, y, str(0), str(sim)])
-                    # print('oov')
-                    pass
-        if len(results) <= 2:
-            return -1, cnt_found_pairs_total, []
+                    results.append((self.default_distance, similarity))
+                    details.append([word_1, word_2, float(self.default_distance), float(similarity)])
+        if len(results) <= self.minimum_found_pairs_number:
+            details = []
+            return self.default_result_value, found_pairs_number, details
         actual, expected = zip(*results)
-        # print(actual)
-        return spearmanr(actual, expected)[0], cnt_found_pairs_total, details
+        return spearmanr(actual, expected), found_pairs_number, details
 
     def read_single_dataset(self, path_to_dir, file_name):
         dataset_name, file_extension = os.path.splitext(file_name)
@@ -94,26 +103,29 @@ class Similarity(Benchmark):
                 datasets[dataset_name][type] = dataset_data
         return datasets
 
-    def make_metadata_dict(self, metadata, found_pairs, benchmark_len, dataset_name, embeddings_metadata):
-        experiment_setup = {}
+    @classmethod
+    def make_metadata_dict(cls, metadata, found_pairs, benchmark_len, dataset_name, embeddings_metadata):
+        experiment_setup = dict()
         experiment_setup['cnt_found_pairs_total'] = found_pairs
         experiment_setup['cnt_pairs_total'] = benchmark_len
         experiment_setup['embeddings'] = embeddings_metadata
-        experiment_setup['category'] = "default"
+        experiment_setup['category'] = 'default'
         experiment_setup['dataset'] = dataset_name
-        experiment_setup['method'] = "cosine_distance"
+        experiment_setup['method'] = 'cosine_distance'
         experiment_setup['language'] = metadata['language']
         experiment_setup['description'] = metadata['description']
         experiment_setup['version'] = metadata['version']
-        experiment_setup['measurement'] = "spearman"
+        experiment_setup['measurement'] = 'spearman'
         experiment_setup['task'] = metadata['task']
         experiment_setup['timestamp'] = datetime.datetime.now().isoformat()
         return experiment_setup
 
-    def make_result(self, result, details, metadata_dict):
-        out = {}
+    @classmethod
+    def make_result(cls, result, details, metadata_dict):
+        out = dict()
         out["experiment_setup"] = metadata_dict
-        out['result'] = result
+        out['correlation'] = result.correlation
+        out['p-value'] = result.pvalue
         out['details'] = details
         return out
 
@@ -133,6 +145,5 @@ class Similarity(Benchmark):
     def get_result(self, embeddings, path_dataset):
         if self.normalize:
             embeddings.normalize()
-
         results = self.run(embeddings, path_dataset)
         return results
