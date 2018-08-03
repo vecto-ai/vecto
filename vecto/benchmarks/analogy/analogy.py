5e110e202be41a820da31911862d62a5991ef7a3
Aleksandr Drozd <alexander.drozd@gmail.com>
2018-08-03 14:16:40 +0900

    cleanup

--- vecto/benchmarks/analogy/analogy.py
+++ vecto/benchmarks/analogy/analogy.py
@@ -298,7 +298,6 @@ class Analogy(Benchmark):
             self.embs.normalize()
         self.embs.cache_normalized_copy()
 
-        # dir_tests = os.path.join(path_dataset)
         results = []
         from vecto.data import Dataset
         dataset = Dataset(path_dataset)
