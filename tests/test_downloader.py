91c145751c433084ab60e850680e64c22b30a853
bakarov <amirbakarov@gmail.com>
2018-09-23 20:38:53 +0300

    add tests for downloader

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -6,5 +6,23 @@ from vecto.downloader import *
 
 class Tests(unittest.TestCase):
 
-    def test_init(self):
-        d = Downloader()
+    def test_downloader(self):
+        downloader = Downloader()
+        downloader.fetch_metadata()
+
+    def test_dir_structure(self):
+        downloader = Downloader()
+        downloader.fetch_metadata()
+        downloader.update_directory_structure()
+        dir_structure = downloader.get_resources()
+        self.assertEquals(len(list(dir_structure.keys())), 1)
+        self.assertEquals(list(dir_structure.keys())[0], 'resources')
+        self.assertEquals(len(list(dir_structure.resources.datasets)), 3)
+
+    def test_resource_fetching(self):
+        downloader = Downloader()
+        downloader.fetch_metadata()
+        downloader.update_directory_structure()
+        dir_structure = downloader.get_resources()
+        downloader.download_resource(dir_structure.resources.datasets.BATS)
+
