639a390da2df0ff81718291cb2efc59099fb3ebd
bakarov <amirbakarov@gmail.com>
2018-11-18 17:43:25 +0300

    update tests

--- tests/test_downloader.py
+++ tests/test_downloader.py
@@ -8,21 +8,21 @@ class Tests(unittest.TestCase):
 
     def test_downloader(self):
         downloader = Downloader()
-        downloader.fetch_metadata()
+        downloader.fetch_metadata(replace=True)
 
     def test_dir_structure(self):
         downloader = Downloader()
-        downloader.fetch_metadata()
+        downloader.fetch_metadata(replace=True)
         downloader.update_directory_structure()
         dir_structure = downloader.get_resources()
         self.assertEquals(len(list(dir_structure.keys())), 1)
         self.assertEquals(list(dir_structure.keys())[0], 'resources')
-        self.assertEquals(len(list(dir_structure.resources.datasets)), 3)
+        self.assertEquals(len(list(dir_structure.resources.evaluation.intrinsic)), 2)
 
     def test_resource_fetching(self):
         downloader = Downloader()
-        downloader.fetch_metadata()
+        downloader.fetch_metadata(replace=True)
         downloader.update_directory_structure()
         dir_structure = downloader.get_resources()
-        downloader.download_resource(dir_structure.resources.datasets.BATS)
+        downloader.download_resource(dir_structure.resources.evaluation.intrinsic.analogy.en.BATS)
 
