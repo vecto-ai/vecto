5c820f04734f8336ca5921556665d1f8eb9b3ecc
Aleksandr Drozd <alexander.drozd@gmail.com>
2018-12-06 16:28:08 +0900

    fix apy reference autosummary

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,6 +53,8 @@ extensions = [
     'sphinx.ext.viewcode',
 ]
 
+autosummary_generate = True 
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
