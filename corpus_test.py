1182fae09de2b6afb708690cf842f1abc048f207
Sasha <alexander.drozd@gmail.com>
2020-12-16 17:29:57 +0900

    corpus testfile

---
+++ corpus_test.py
@@ -0,0 +1,19 @@
+from vecto.corpus import ViewCorpus
+
+path = "./tests/data/corpora/"
+corpus = ViewCorpus(path)
+corpus.load_dir_strucute()
+# rank 0 creates corpus from dir
+# corpus has inside all file list and sizes
+# use manually splits sends metadata of corpus : tree of dirs and files with uncompressed sizes to all workers
+# otehr workers create corpora from that metadata using special service method like __from_metadata
+# to avoid exessive file IO
+
+# for time being - everybody just reads from FS
+
+# # view = corpus.view(start_percent, end_pecent)
+# print(corpus)
+# iter_token = corpus.get_line_iterator()
+# for s in iter_token:
+#     print(s)
+
